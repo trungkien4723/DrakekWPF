7f8fb54fc21d3f26b4e81ceb750d50143e197da3
ntrungkien4723 <ntrungkien4723@gmail.com>
2025-02-02 23:33:11 +0700

    feat(Main Window) add basic features

---
+++ Controller/Product.xaml.cs
@@ -0,0 +1,21 @@
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace drakek.Controller
+{
+    public partial class Product : UserControl
+    {
+        public Product()
+        {
+            InitializeComponent();
+        }
+    }
+}
