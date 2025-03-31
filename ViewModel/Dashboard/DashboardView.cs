8adfc6aec502719106b1466eb3c5978612a4f1a3
ntrungkien4723 <ntrungkien4723@gmail.com>
2025-03-31 17:58:59 +0700

    feat(Dashboard): add counters to dashboard

--- ViewModel/Dashboard/DashboardView.cs
+++ ViewModel/Dashboard/DashboardView.cs
@@ -14,10 +14,21 @@ namespace drakek.ViewModel
 {
     public partial class DashboardView : UserControl
     {
+        
+        public int ordersCount{ get; set; }
+        public int productsCount{ get; set; }
+        public int customersCount{ get; set; }
         private DashboardController dashboardController = new DashboardController();
+        private OrderController orderController = new OrderController();
+        private ProductController productController = new ProductController();
+        private CustomerController customerController = new CustomerController();
         public DashboardView()
         {
             InitializeComponent();
+            DataContext = this;
+            ordersCount = orderController.getAllOrders().Count;
+            productsCount = productController.getAllProducts().Count;
+            customersCount = customerController.getAllCustomers().Count;
         }
     }
 }
